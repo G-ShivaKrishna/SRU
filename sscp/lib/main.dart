3bfd9936a0918fc2abfb679723d328c5fe149e9c
G-ShivaKrishna <reddyshivakrishna43@gmail.com>
2026-01-14 21:43:19 +0530

    Enhance Firebase initialization by adding auth state verification

--- sscp/lib/main.dart
+++ sscp/lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 import 'app/app.dart';
-
+//emunnave pilla
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   try {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
+    // Verify Firebase connection
+    await FirebaseAuth.instance.authStateChanges().first;
     runApp(const MyApp());
   } catch (e) {
     runApp(FirebaseErrorApp(error: e.toString()));
