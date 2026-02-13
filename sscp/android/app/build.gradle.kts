15cce6766cea9c94727b155e43be2552d81c41cf
G-ShivaKrishna <reddyshivakrishna43@gmail.com>
2026-02-13 22:22:21 +0530

    fix: Update NDK version to 29.0.14206865 in build.gradle.kts

--- sscp/android/app/build.gradle.kts
+++ sscp/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.sscp_new"
     compileSdk = 35
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "29.0.14206865"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
